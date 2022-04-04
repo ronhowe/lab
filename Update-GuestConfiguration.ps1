78edb6658dc9c2e1d09a663b9a486ecd0f23a6ce
Ron Howe <ronhowe@hotmail.com>
2022-04-04 14:46:32 -0400

    Added Update-GuestConfiguration.ps1

---
+++ Update-GuestConfiguration.ps1
@@ -0,0 +1,10 @@
+#requires -RunAsAdministrator
+#requires -PSEdition Desktop
+
+Set-Location -Path $PSScriptRoot
+
+$AdministratorCredential = Get-Credential -Message "Enter Administrator Credential" -Username "Administrator"
+
+$ComputerNames = @("DC01", "SQL01", "USER01", "WEB01")
+
+$ComputerNames | .\Invoke-GuestConfiguration.ps1 -Credential $AdministratorCredential -Wait -Verbose
