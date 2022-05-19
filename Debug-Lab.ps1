2828a01ddf7e52a78a2c7c033cc9f71545034a58
Ron Howe <ronhowe@hotmail.com>
2022-05-19 11:18:59 -0400

    Updated Lab Debug Script

--- Debug-Lab.ps1
+++ Debug-Lab.ps1
@@ -1,5 +1,38 @@
-$AdministratorCredential = Get-Credential -Message "Enter Administrator Credential" -Username "Administrator"
+#requires -RunAsAdministrator
 
-$ComputerNames = @("DC41", "SQL41", "USER41", "WEB41")
+# Configure preferences.
+$ProgressPreference = "SilentlyContinue"
 
-$ComputerNames | .\Invoke-GuestConfiguration.ps1 -Credential $AdministratorCredential -Wait
+# Import module(s).
+Import-Module -Name "Hyper-V"
+
+# Set frequently used variables.
+$Credential = Get-Credential -Message "Enter Administrator Credential"
+$ComputerName = "WINDOWS"
+
+# TODO Create Virtual Machine
+
+# Get the computer name.
+Invoke-Command -VMName $ComputerName -Credential $Credential -ScriptBlock { hostname }
+
+# Rename the computer.
+Invoke-Command -VMName $ComputerName -Credential $Credential -ScriptBlock { Rename-Computer -NewName $using:ComputerName -Restart -Force }
+
+# Allow PING (ICMP) through the Windows Firewall.
+Invoke-Command -VMName $ComputerName -Credential $Credential -ScriptBlock { New-NetFirewallRule -DisplayName “Allow inbound ICMPv4” -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow }
+Invoke-Command -VMName $ComputerName -Credential $Credential -ScriptBlock { New-NetFirewallRule -DisplayName “Allow inbound ICMPv6” -Direction Inbound -Protocol ICMPv6 -IcmpType 8 -Action Allow }
+
+# PING the computer.
+Test-NetConnection -ComputerName $ComputerName -WarningAction SilentlyContinue
+
+# Get a remote session to the computer.
+$Session = New-PSSession -ComputerName $ComputerName -Credential $Credential
+
+# Enter remote session.
+Enter-PSSession -Session $Session
+
+# Close the remote session.
+Remove-PSSession -Session $Session
+
+# Reboot the computer.
+Restart-Computer -ComputerName $ComputerName -Credential $Credential -Force -Wait
