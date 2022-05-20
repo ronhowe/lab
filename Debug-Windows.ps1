e49f2e3f7bfbfd8cbb95e02af16b6abf85edcac1
Ron Howe <ronhowe@hotmail.com>
2022-05-20 12:45:02 -0400

    Replaced Lab with Windows Script + Refactoring

---
+++ Debug-Windows.ps1
@@ -0,0 +1,28 @@
+# Configure preferences.
+$ProgressPreference = "SilentlyContinue"
+
+# Import modules.
+Import-Module -Name "Hyper-V"
+
+# Set frequently used variables.
+$Credential = Get-Credential -Message "Enter Administrator Credential"
+$ComputerName = Read-Host -Prompt "Enter Computer Name"
+
+# Allow PING (ICMP) through the Windows Firewall.
+Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock { New-NetFirewallRule -DisplayName “Allow inbound ICMPv4” -Direction Inbound -Protocol ICMPv4 -IcmpType 8 -Action Allow }
+Invoke-Command -ComputerName $ComputerName -Credential $Credential -ScriptBlock { New-NetFirewallRule -DisplayName “Allow inbound ICMPv6” -Direction Inbound -Protocol ICMPv6 -IcmpType 8 -Action Allow }
+
+# PING the computer.
+Test-NetConnection -ComputerName $ComputerName -WarningAction SilentlyContinue
+
+# Get a remote session to the computer.
+$Session = New-PSSession -ComputerName $ComputerName -Credential $Credential
+
+# Enter remote session.
+Enter-PSSession -Session $Session
+
+# Close the remote session.
+Remove-PSSession -Session $Session
+
+# Reboot the computer.
+Restart-Computer -ComputerName $ComputerName -Credential $Credential -Force -Wait
