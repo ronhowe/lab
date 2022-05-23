ec433c1af729338ffe276a1eb54a05f15a13cac5
Ron Howe <ronhowe@hotmail.com>
2022-05-23 11:47:12 -0400

    Added SSH Call in Linux Script

--- Debug-Linux.ps1
+++ Debug-Linux.ps1
@@ -9,3 +9,6 @@ $ComputerName = Read-Host -Prompt "Enter Computer Name"
 
 # https://pitstop.manageengine.com/portal/en/kb/articles/how-to-get-the-ip-address-for-hyper-v-linux-vm-in-apm
 # sudo apt-get install linux-azure
+
+# ssh administrator@linux-vm
+ssh "$($($Credential).Username)@$($ComputerName)"
