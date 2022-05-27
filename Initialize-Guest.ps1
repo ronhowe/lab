8d6a7f1d5695e1377637f4cdecd8137eebdc05c9
Ron Howe <ronhowe@hotmail.com>
2022-05-27 16:03:23 -0400

    Set Static IP in Initialize Guest Script

--- Initialize-Guest.ps1
+++ Initialize-Guest.ps1
@@ -38,13 +38,28 @@ process {
             Set-LocalUser -Name "Administrator" -Password $AdministratorCredential.Password
 
             Write-Output "Setting Network Profile to Private on $env:COMPUTERNAME"
-            Get-NetAdapter -InterfaceDescription "Microsoft Hyper-V Network Adapter" | Set-NetConnectionProfile -NetworkCategory Private
+            Get-NetAdapter -Name "Ethernet" | Set-NetConnectionProfile -NetworkCategory Private
 
             Write-Output "Enabling WinRM on $env:COMPUTERNAME"
             Start-Process -FilePath "winrm" -ArgumentList @("quickconfig", "-force")
 
             Write-Output "Setting Execution Policy to Unrestricted on $env:COMPUTERNAME"
             Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope LocalMachine -Force
+
+            Write-Output "Setting Static IP Configuration on $env:COMPUTERNAME"
+            $IpAddress =
+            switch ($env:COMPUTERNAME) {
+                "DC-VM" { "192.168.0.10" }
+                "SQL-VM" { "192.168.0.20" }
+                "USER-VM" { "192.168.0.40" }
+                "WEB-VM" { "192.168.0.30" }
+                default { throw }
+            }
+            $InterfaceIndex = $(Get-NetAdapter -Name "Ethernet").ifIndex
+            Remove-NetIPAddress -InterfaceIndex $InterfaceIndex -Confirm:$false
+            Remove-NetRoute -InterfaceIndex $InterfaceIndex -Confirm:$false
+            New-NetIPAddress -IPAddress $IpAddress -AddressFamily IPv4 -PrefixLength "24" -InterfaceIndex $InterfaceIndex -DefaultGateway "192.168.0.1" | Out-Null
+            Set-DnsClientServerAddress -InterfaceIndex $InterfaceIndex -ServerAddresses ("192.168.1.1") | Out-Null
         }
         end {
         }
